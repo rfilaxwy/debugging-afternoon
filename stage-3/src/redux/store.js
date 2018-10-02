aeebbb0e48aa893c628b9df7d0dc127071b66cf7
rfilaxwy <ryanfilaxwylie@gmail.com>
2018-10-02 07:28:05 -0600

    Stage 3 complete

--- stage-3/src/redux/store.js
+++ stage-3/src/redux/store.js
@@ -1,4 +1,4 @@
 import reducer from './reducer';
 import reduxPromiseMiddleware from 'redux-promise-middleware';
-
+import {createStore, applyMiddleware} from 'redux';
 export default createStore(reducer, applyMiddleware(reduxPromiseMiddleware()));
